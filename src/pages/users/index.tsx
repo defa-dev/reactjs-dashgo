712650cc3334f9ed277c6be8c545ace33c0c4e14
Felipe Defacio <phelipe2208@gmail.com>
2022-03-24 09:12:12 -0300

    chore: use axios instead fetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,29 +24,32 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery(
+    "users",
+    async () => {
+      const { data } = await api.get("users");
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-br", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users;
-  }, {
-    staleTime: 1000 * 5,
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5,
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -70,6 +73,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -117,12 +123,11 @@ export default function UserList() {
                           <Box>
                             <Text fontWeight="bold">{user.name}</Text>
                             <Text fontSize="sm" color="gray.300">
-                            {user.email}
+                              {user.email}
                             </Text>
                           </Box>
                         </Td>
                         {isWideVersion && <Td>{user.createdAt}</Td>}
-                        
                       </Tr>
                     );
                   })}
